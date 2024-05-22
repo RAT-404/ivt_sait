e2c28f73347efc787030477166a468855e4f6da5
Mack <mangalaxy433@gmail.com>
2024-05-22 16:18:45 +0300

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+
+app.mount("/images", StaticFiles(directory="templates/images"), name="images")
+app.mount('/css', StaticFiles(directory="templates/css"), name='css')
+app.mount('/vec_img', StaticFiles(directory="templates/vec_img"), name='vec_img')
+app.mount('/scss', StaticFiles(directory="templates/scss"), name='scss')
+
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get('/')
+async def root(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
+

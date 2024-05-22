5264507e02ab7d0523cc15b8c5594bcdd34d2afd
Mack <mangalaxy433@gmail.com>
2024-05-22 16:31:00 +0300

    refresh project structure, ivt-sait -> app/temlates

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -15,7 +15,7 @@ app.mount('/scss', StaticFiles(directory="templates/scss"), name='scss')
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get('/')
+@app.get('/', response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse(request=request, name="index.html")
 
